59d62cbe2fd840db4d359ea9de56a9a6267939f1
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-19 10:36:14 +0300

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,35 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+    // Update state by passing the array of items to setItems  
+    useEffect(() => {
+      fetch("http://localhost:4000/items")
+        .then((r) => r.json())
+        // .then((items) =>console.log(items));
+        .then((items) => setItems(items))
+    }, []);
+
+    // ...rest of component
+
+     // add this callback function
+
+     function handleUpdateItem(updatedItem) {
+      const updatedItems = items.map((item) => {
+        if (item.id === updatedItem.id) {
+          return updatedItem;
+        } else {
+          return item;
+        }
+      });
+      setItems(updatedItems);
+    }
+
+  function handleAddItem(newItem) {
+    // console.log("In ShoppingList:", newItem);
+    setItems([...items, newItem]);
+  }
+
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -17,16 +46,25 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
+  function handleDeleteItem(deletedItem) {
+    // console.log("In ShoppingCart:", deletedItem);
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+     {/* add the onAddItem prop! */}
+     <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          {/* pass it as a prop to Item */}
+          {itemsToDisplay.map((item) => (
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
